8d273ae6563e56330ceb58f8735cde05d2bd756e
Nicholas Bishop <nbishop@nbishop.net>
2023-07-30 17:21:03 -0400

    sbat: Rework API
    This is a large change to the public API. The overall goal of the change
    is to allow a fully owned version of the API (when the `alloc` feature
    is enabled).
    
    * The `ImageSbat` and `RevocationSbat` traits have been removed.
    
    * `ImageSbat` and `RevocationSbat` are now structs containing the raw
      ASCII CSV data. As before, the `parse` methods still fully validate
      the data. These are DST types, so they must be passed around by
      reference (similar to passing around an `&str`). This requires a small
      amount of unsafe code to create the DSTs.
    
    * Access to the entries and revoked components is now provided by an
      iterator rather than returning a slice. This means that parsing is no
      longer cached; each time the caller iterates through entries or
      components it is redoing some of the CSV parsing. This should not
      matter much in practice since SBAT data is intended to be small. You
      should never have hundreds of items in either the revocation data or
      the image metadata, as that would defeat the purpose of SBAT.
    
    * The `ImageSbatArray` and `RevocationSbatArray` types have been
      removed, use `&ImageSbat` and `&RevocationSbat` instead.
    
    * The `ImageSbatVec` and `RevocationSbatVec` types have been replaced by
      `ImageSbatOwned` and `RevocationSbatOwned`. These new types are fully
      owned, i.e. they don't take a lifetime parameter. Internally they just
      contain an `AsciiString`.
    
    The API no longer includes a way to build SBAT objects dynamically,
    everything is parsed from existing strings instead. New builder types
    can be added in the future if a need is found for that.

--- sbat/tests/example.rs
+++ sbat/tests/example.rs
@@ -1,58 +1,25 @@
-use sbat::{
-    ImageSbat, ImageSbatArray, ParseError, RevocationSbat, RevocationSbatArray,
-    ValidationResult,
-};
-#[cfg(feature = "alloc")]
-use sbat::{ImageSbatOwned, RevocationSbatOwned};
+use sbat::{ImageSbat, ParseError, RevocationSbat, ValidationResult};
 
-const IMAGE_SBAT_A1: &[u8] = b"sbat,1\nCompA,1";
-const IMAGE_SBAT_A2: &[u8] = b"sbat,1\nCompA,2";
-const REVOCATION_SBAT: &[u8] = b"sbat,1,2021030218
-CompA,2";
+fn main() -> Result<(), ParseError> {
+    let image_sbat_a1 = b"sbat,1\nCompA,1";
+    let image_sbat_a2 = b"sbat,1\nCompA,2";
+    let revocation_sbat = b"sbat,1,2021030218\nCompA,2";
 
-/// This example uses fixed-size array types that do not allocate.
-#[test]
-fn example_fixed_size() -> Result<(), ParseError> {
     // Parse the image and revocation SBAT.
-    let image_sbat = ImageSbatArray::<10>::parse(IMAGE_SBAT_A1)?;
-    let revocations = RevocationSbatArray::<10>::parse(REVOCATION_SBAT)?;
+    let image_sbat = ImageSbat::parse(image_sbat_a1)?;
+    let revocations = RevocationSbat::parse(revocation_sbat)?;
 
     // Check that the image is revoked.
     assert_eq!(
-        revocations.validate_image(&image_sbat),
-        ValidationResult::Revoked(*image_sbat.entries().last().unwrap()),
+        revocations.validate_image(image_sbat),
+        ValidationResult::Revoked(image_sbat.entries().last().unwrap()),
     );
 
-    // Change the image's CompA generation to 1 and verify that it is no
+    // Change the image's CompA generation to 2 and verify that it is no
     // longer revoked.
-    let image_sbat = ImageSbatArray::<10>::parse(IMAGE_SBAT_A2)?;
+    let image_sbat = ImageSbat::parse(image_sbat_a2)?;
     assert_eq!(
-        revocations.validate_image(&image_sbat),
-        ValidationResult::Allowed,
-    );
-
-    Ok(())
-}
-
-/// This example uses `Vec`-like types that dynamically allocate.
-#[cfg(feature = "alloc")]
-#[test]
-fn example_vec() -> Result<(), ParseError> {
-    // Parse the image and revocation SBAT.
-    let image_sbat = ImageSbatOwned::parse(IMAGE_SBAT_A1)?;
-    let revocations = RevocationSbatOwned::parse(REVOCATION_SBAT)?;
-
-    // Check that the image is revoked.
-    assert_eq!(
-        revocations.validate_image(&image_sbat),
-        ValidationResult::Revoked(*image_sbat.entries().last().unwrap()),
-    );
-
-    // Change the image's CompA generation to 1 and verify that it is no
-    // longer revoked.
-    let image_sbat = ImageSbatOwned::parse(IMAGE_SBAT_A2)?;
-    assert_eq!(
-        revocations.validate_image(&image_sbat),
+        revocations.validate_image(image_sbat),
         ValidationResult::Allowed,
     );
 
