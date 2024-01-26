c2c1eddf07eb1b2dd4f58e87d089f6f19f6d0a01
Nicholas Bishop <nicholasbishop@google.com>
2024-01-26 15:51:37 -0500

    fuzz: Test the alloc structs
    Use ImageSbatOwned and RevocationSbatOwned to increase the fuzz
    coverage. These structs mostly delegate to ImageSbat and RevocationSbat,
    so this should strictly increase coverage.

--- fuzz/fuzz_targets/combo.rs
+++ fuzz/fuzz_targets/combo.rs
@@ -1,15 +1,16 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
-use sbat::{ImageSbat, RevocationSbat};
+use sbat::{ImageSbatOwned, RevocationSbatOwned};
 
 // Generate both ImageSbat and RevocationSbat so they can be tested
 // together.
 fuzz_target!(|data: (&[u8], &[u8])| {
     let (image_data, revocation_data) = data;
+
     if let (Ok(image), Ok(revocations)) = (
-        ImageSbat::parse(image_data),
-        RevocationSbat::parse(revocation_data),
+        ImageSbatOwned::parse(image_data),
+        RevocationSbatOwned::parse(revocation_data),
     ) {
         let _ = image.as_csv();
 
@@ -17,7 +18,7 @@ fuzz_target!(|data: (&[u8], &[u8])| {
         let _ = revocations.date();
         for _ in revocations.revoked_components() {}
 
-        let _ = revocations.validate_image(image);
+        let _ = revocations.validate_image(&image);
         for entry in image.entries() {
             let _ = revocations.is_component_revoked(&entry.component);
         }
